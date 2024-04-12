8bc1d9488dc3e99e6fb07afbccb7fedd335767dd
RPathare <rutupathare123@gmail.com>
2024-04-12 09:49:59 +0530

    first commit

---
+++ tns1/src/tns1/tns2.java
@@ -0,0 +1,14 @@
+package tns1;
+
+public class tns2 {
+	public static void main(String[] args) {
+		int a = 10;
+		int b=4;
+		if(a>b) {
+			System.out.println("greater number");
+		}
+		else {
+			System.out.println("smaller number");
+		}
+	}
+}
